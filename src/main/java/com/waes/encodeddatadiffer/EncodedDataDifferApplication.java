8f4b72546ca88ca4dff1b57b2f4fd85d4a42d106
Diego Rojas <darguelles.rojas91@gmail.com>
2021-04-01 17:16:06 -0300

    Basic project setup

---
+++ src/main/java/com/waes/encodeddatadiffer/EncodedDataDifferApplication.java
@@ -0,0 +1,13 @@
+package com.waes.encodeddatadiffer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncodedDataDifferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncodedDataDifferApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/waes/encodeddatadiffer/EncodedDataDifferApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waes.encodeddatadiffer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncodedDataDifferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
