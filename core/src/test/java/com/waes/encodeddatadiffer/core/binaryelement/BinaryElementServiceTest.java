9c38d689141bb5e4b65fb701858bbc0684d62f37
Diego Rojas <darguelles.rojas91@gmail.com>
2021-04-04 20:48:46 -0300

    Include test cases for service

--- core/src/test/java/com/waes/encodeddatadiffer/core/binaryelement/BinaryElementServiceTest.java
+++ core/src/test/java/com/waes/encodeddatadiffer/core/binaryelement/BinaryElementServiceTest.java
@@ -1,2 +1,47 @@
-package com.waes.encodeddatadiffer.core.binaryelement;public class TestBinaryElementService {
+package com.waes.encodeddatadiffer.core.binaryelement;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class BinaryElementServiceTest {
+
+    @Test
+    void shouldSaveBinaryElementWithJustOneSideProvided() {
+    }
+
+    @Test
+    void shouldUpdateBinaryElement() {
+    }
+
+    @Test
+    void shouldRetrieveBinaryElementByID() {
+    }
+
+    @Test
+    void whenSavingBinaryElementShouldThrowInvalidDataEncryptionExceptionIfElementIsNotBase64Encoded() {
+    }
+
+    @Test
+    void shouldReturnEqualStatusIfBothElementSidesAreTheSame() {
+    }
+
+    @Test
+    void shouldReturnDifferentStatusIfBothElementSidesAreDifferentInSize() {
+    }
+
+    @Test
+    void shouldReturnDifferencesListWhenElementHasSidesWithEqualSizeAndDifferentData() {
+    }
+
+    @Test
+    void shouldThrowMissingSideExceptionIfOneElementSideIsNull() {
+    }
+
+    @Test
+    void shouldThrowMissingSideExceptionIfOneElementSideIsEmpty() {
+    }
+
+
 }
