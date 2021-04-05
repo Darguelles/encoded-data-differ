cb1f61269167254d07cda9467c45fbcd53fc6566
Diego Rojas <darguelles.rojas91@gmail.com>
2021-04-05 02:25:16 -0300

    Configured in memory data source

---
+++ application/src/main/java/com/waes/encodeddatadiffer/repository/BinaryElementMemoryRepository.java
@@ -0,0 +1,8 @@
+package com.waes.encodeddatadiffer.repository;
+
+import com.waes.encodeddatadiffer.core.binaryelement.BinaryElement;
+import org.springframework.data.repository.CrudRepository;
+
+public interface BinaryElementMemoryRepository extends CrudRepository<BinaryElement, Long> {
+
+}
